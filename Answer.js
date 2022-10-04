4717bdf2f4f3a17ef8932b9f03c73187abd1b799
mnubhan <76425959+mnubhan@users.noreply.github.com>
2022-10-04 21:51:17 +0800

    Create Answer.js

---
+++ Answer.js
@@ -0,0 +1,15 @@
+const expandedForm = n => n.toString()
+                            .split("")
+                            .reverse()
+                            .map( (a, i) => a * Math.pow(10, i))
+                            .filter(a => a > 0)
+                            .reverse()
+                            .join(" + ");
+                    
+function expandedForm(num) {
+  return String(num)
+          .split("")
+          .map((num, index, arr) => num + "0".repeat(arr.length - index -1 ))
+          .filter((num) => Number(num) != 0)
+          .join(" + ")
+}
